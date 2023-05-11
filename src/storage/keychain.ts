58e906c63bc4d0a46b5ce2343c146dac0a55dc66
obasilakis <vasileios.georgopoulos@gmail.com>
2023-05-11 18:32:18 +0300

    downgrade to SECURITY_SOFTWARE when hardware not available

--- src/storage/keychain.ts
+++ src/storage/keychain.ts
@@ -2,19 +2,32 @@ import * as Keychain from 'react-native-keychain';
 import { ACCESS_CONTROL, ACCESSIBLE, AUTHENTICATION_TYPE, SECURITY_LEVEL, STORAGE_TYPE } from 'react-native-keychain';
 
 type KeychainItem = { key: string; value: string };
+
+const defaultOptions = (key: string) => ({
+    accessControl: ACCESS_CONTROL.DEVICE_PASSCODE,
+    // ios only
+    accessible: ACCESSIBLE.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY,
+    // ios only
+    authenticationType: AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
+    // android only
+    securityLevel: SECURITY_LEVEL.SECURE_HARDWARE,
+    service: key,
+    // android only
+    storage: STORAGE_TYPE.AES
+})
 export const storeItem = async ({ key, value }: KeychainItem) => {
-    await Keychain.setGenericPassword('Quaipay', value, {
-        accessControl: ACCESS_CONTROL.DEVICE_PASSCODE,
-        // ios only
-        accessible: ACCESSIBLE.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY,
-        // ios only
-        authenticationType: AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
-        // android only
-        securityLevel: SECURITY_LEVEL.SECURE_HARDWARE,
-        service: key,
-        // android only
-        storage: STORAGE_TYPE.AES
-    })
+    try {
+        await Keychain.setGenericPassword('Quaipay', value, defaultOptions(key))
+    } catch (error: any) {
+        console.log(error);
+        if (error.message.includes('Insufficient security level')) {
+            try {
+                await Keychain.setGenericPassword('Quaipay', value, { ...defaultOptions(key), securityLevel: SECURITY_LEVEL.SECURE_SOFTWARE });
+            } catch (error: any) {
+                console.log(error);
+            }
+        }
+    }
 
     const retrievedValue = await retrieveStoredItem(key);
     if (value !== retrievedValue) {
@@ -33,7 +46,7 @@ export const retrieveStoredItem = async (key: string) => {
         authenticationType: AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
         securityLevel: SECURITY_LEVEL.SECURE_HARDWARE,
         service: key,
-        storage: STORAGE_TYPE.AES
+        storage: STORAGE_TYPE.RSA
     });
     if (credentials) {
         return credentials.password;

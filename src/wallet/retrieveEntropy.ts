71e5d24fa8ceb67deb07c85258e7068c64d33f4a
obasilakis <vasileios.georgopoulos@gmail.com>
2023-05-18 15:26:39 +0300

    add retrieve entropy method

---
+++ src/wallet/retrieveEntropy.ts
@@ -0,0 +1,13 @@
+import { retrieveStoredItem } from '../storage/keychain';
+import buffer from 'buffer';
+
+export async function retrieveEntropy() {
+  const retrievedEntropy = await retrieveStoredItem('entropy');
+  if (retrievedEntropy === null) {
+    throw new Error('Entropy not found in keychain');
+  }
+  return Uint8Array.from(
+    buffer.Buffer.from(retrievedEntropy, 'base64').toString('binary'),
+    c => c.charCodeAt(0),
+  );
+}

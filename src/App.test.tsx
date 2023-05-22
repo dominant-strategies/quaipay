358b92d5a61346a6d55fcc1f761ad8f06fec747f
obasilakis <vasileios.georgopoulos@gmail.com>
2023-05-22 11:46:33 +0300

    add user interaction test

---
+++ src/App.test.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react-native';
+import App from './App';
+import { act } from 'react-test-renderer';
+
+jest.mock('react-native-geolocation-service', () => {});
+jest.mock('react-native-permissions', () => {});
+jest.mock('./wallet/setUpWallet', () => ({
+  setUpWallet: jest.fn(),
+}));
+
+describe.only('Testing react navigation', () => {
+  test('app goes from welcome to username/profile picture screen', async () => {
+    const component = <App />;
+
+    render(component);
+
+    const header = await screen.findByText('Welcome to\nQuaiPay.');
+    expect(header).toBeOnTheScreen();
+    act(() => {
+      const setupButton = screen.getByText('Setup');
+      fireEvent.press(setupButton);
+    });
+
+    await new Promise(r => setTimeout(r, 5000));
+
+    const chooseUserName = screen.getByText(
+      'Choose a\nusername and\nprofile picture',
+    );
+    expect(chooseUserName).toBeOnTheScreen();
+  }, 10000);
+});

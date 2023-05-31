e2a4d7017cd04a02c3d06e0dc984cbbda439a13c
hsyndeniz <huseyin.deniz1@hotmail.com>
2023-05-31 13:00:30 +0300

    update i18n

--- src/App.tsx
+++ src/App.tsx
@@ -5,37 +5,10 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { initReactI18next } from 'react-i18next';
-import i18n from 'i18next';
 import OnboardingStack from './onboarding/OnboardingStack';
 import MainStack from './main/MainStack';
 import Loader from './shared/Loader';
-import home_en from './shared/locales/en/home.json';
-import home_de from './shared/locales/de/home.json';
-import onboarding_en from './shared/locales/en/onboarding.json';
-import onboarding_de from './shared/locales/de/onboarding.json';
-
-i18n.use(initReactI18next).init({
-  resources: {
-    en: {
-      translation: {
-        home: home_en,
-        onboarding: onboarding_en,
-      },
-    },
-    de: {
-      translation: {
-        home: home_de,
-        onboarding: onboarding_de,
-      },
-    },
-  },
-  lng: 'en',
-  fallbackLng: 'en',
-  interpolation: {
-    escapeValue: false,
-  },
-});
+import './shared/locales';
 
 const Tab = createBottomTabNavigator();
 

8fc9b26a65bd8bcc426743f6f9f81fb2dcb02578
juam <juanmanso@gmail.com>
2023-06-02 17:38:55 +0200

    feat: add input change logic

--- src/main/home/receive/ReceiveAmountInputScreen/hooks.ts
+++ src/main/home/receive/ReceiveAmountInputScreen/hooks.ts
@@ -2,11 +2,30 @@ import { useState } from 'react';
 
 import { EUnit } from './types';
 
+const INITIAL_AMOUNT = '0';
+
 export const useReceiveInput = () => {
-  const [amount, setAmount] = useState('0.00');
+  const [amount, setAmount] = useState(INITIAL_AMOUNT);
   const [unit, setUnit] = useState(EUnit.USD);
 
-  const onInputChange = setAmount;
+  const onInputChange = (value: string) => {
+    const prevValue = value.slice(0, -1);
+    const newValue = value[value.length - 1];
+
+    if (newValue === '.') {
+      return setAmount(value);
+    }
+
+    if (prevValue === INITIAL_AMOUNT) {
+      return setAmount(value[value.length - 1]);
+    }
+
+    if (value === '') {
+      return setAmount(INITIAL_AMOUNT);
+    }
+
+    return setAmount(value);
+  };
 
   const onSwap = () => {
     const result = unit === EUnit.USD ? EUnit.QUAI : EUnit.USD;

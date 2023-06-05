cb278940a627c6f42c0b3e875d1d2513584951a9
juam <juanmanso@gmail.com>
2023-06-05 12:52:10 +0200

    feat: add react-native-localization to detect device's language

--- src/shared/locales/index.ts
+++ src/shared/locales/index.ts
@@ -1,11 +1,15 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { findBestLanguageTag } from 'react-native-localize';
+
 import common_en from './en/common.json';
 import common_de from './de/common.json';
 import home_en from './en/home.json';
 import home_de from './de/home.json';
 import onboarding_en from './en/onboarding.json';
 import onboarding_de from './de/onboarding.json';
+
+const fallbackLng = findBestLanguageTag(['en', 'de'])?.languageTag;
 
 i18n.use(initReactI18next).init({
   resources: {
@@ -24,8 +28,7 @@ i18n.use(initReactI18next).init({
       },
     },
   },
-  lng: 'en',
-  fallbackLng: 'en',
+  fallbackLng: fallbackLng || 'en',
   interpolation: {
     escapeValue: false,
   },

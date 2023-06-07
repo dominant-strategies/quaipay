2a823d68f21ae652d817f65259730e2f5afec54c
juam <juanmanso@gmail.com>
2023-06-07 13:22:47 +0200

    refactor: split index style file into smaller ones

---
+++ src/shared/styles/colors.ts
@@ -0,0 +1,11 @@
+export const styledColors = {
+  normal: '#0066FF',
+  black: '#000000',
+  dark: '#161616',
+  darkGray: '#303030',
+  gray: '#808080',
+  lightGray: '#D4D4D4',
+  light: '#EFEFEF',
+  white: '#FFFFFF',
+  border: '#D5D5D5',
+};
---
+++ src/shared/styles/typography.ts
@@ -0,0 +1,52 @@
+import { StyleSheet } from 'react-native';
+
+export const fontStyle = StyleSheet.create({
+  fontH1: {
+    fontFamily: 'Clash Display Variable',
+    fontSize: 32,
+    fontWeight: '600',
+    lineHeight: 36,
+    letterSpacing: 0.05,
+    textAlign: 'center',
+  },
+  fontH2: {
+    fontFamily: 'Satoshi Variable',
+    fontSize: 24,
+    fontWeight: '600',
+    lineHeight: 32.5,
+    letterSpacing: 0,
+    textAlign: 'center',
+  },
+  fontH3: {
+    fontFamily: 'Satoshi Variable',
+    fontSize: 14,
+    fontWeight: '500',
+    lineHeight: 19,
+    letterSpacing: 0,
+    textAlign: 'center',
+  },
+  fontParagraph: {
+    fontFamily: 'Satoshi Variable',
+    fontSize: 14,
+    fontWeight: '400',
+    lineHeight: 19,
+    letterSpacing: 0,
+    textAlign: 'center',
+  },
+  fontSmallText: {
+    fontFamily: 'Satoshi Variable',
+    fontSize: 12,
+    fontWeight: '400',
+    lineHeight: 16,
+    letterSpacing: 0,
+    textAlign: 'center',
+  },
+  fontSmallTextBold: {
+    fontFamily: 'Satoshi Variable',
+    fontSize: 12,
+    fontWeight: '600',
+    lineHeight: 16,
+    letterSpacing: 0,
+    textAlign: 'left',
+  },
+});

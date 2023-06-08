f33cee0620ff5bd96a19fe47118e34020fa4a5bb
hsyndeniz <huseyin.deniz1@hotmail.com>
2023-06-08 11:55:31 +0300

    add headers to screens

--- src/main/home/send/SendStack.tsx
+++ src/main/home/send/SendStack.tsx
@@ -1,19 +1,42 @@
+/* eslint-disable react/no-unstable-nested-components */
+import React, { useCallback } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigation } from '@react-navigation/native';
+import { Pressable, Text, useColorScheme } from 'react-native';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import SendTipScreen from './SendTip';
 import SendScanScreen from './SendScan';
 import SendAmountScreen from './SendAmount';
 import SendOverviewScreen from './SendOverview';
 import SendConfirmationScreen from './SendConfirmation';
+import { styledColors } from 'src/styles';
 
-export type SendStackStackParamList = {
+export type SendStackParamList = {
   SendScan: undefined;
-  SendAmount: { address: string; amount?: string; username: string };
-  SendOverview: undefined;
-  SendConfirmation: undefined;
+  SendAmount: { address: string; amount?: number; username: string };
+  SendTip: { address: string; amount: number; username: string };
+  SendOverview: { address: string; amount: number; username: string };
+  SendConfirmation: { address: string; amount: number; username: string };
 };
 
-const Stack = createNativeStackNavigator<SendStackStackParamList>();
-const SendStackStack = () => {
+const Stack = createNativeStackNavigator<SendStackParamList>();
+const SendStack = () => {
+  const { t } = useTranslation();
+  const navigation = useNavigation();
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundColor = isDarkMode ? styledColors.black : styledColors.light;
+  const textColor = isDarkMode ? styledColors.white : styledColors.black;
+
+  const textStyle = { color: textColor, fontSize: 24 };
+  const buttonStyle = { backgroundColor, marginLeft: 8 };
+
+  const goBack = useCallback(
+    () => (navigation.canGoBack() ? navigation.goBack() : false),
+    [navigation],
+  );
+
   return (
     <Stack.Navigator initialRouteName="SendScan">
       <Stack.Screen
@@ -23,8 +46,37 @@ const SendStackStack = () => {
       />
       <Stack.Screen
         name="SendAmount"
-        options={{ headerShown: false }}
+        options={{
+          headerStyle: { backgroundColor },
+          headerShadowVisible: false,
+          headerTitleAlign: 'center',
+          headerTitle: () => (
+            <Text style={textStyle}>{t('home.send.label')}</Text>
+          ),
+          headerLeft: () => (
+            <Pressable style={buttonStyle} onPress={goBack}>
+              <FontAwesome5 name="chevron-left" color={textColor} size={24} />
+            </Pressable>
+          ),
+        }}
         component={SendAmountScreen}
+      />
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor },
+          headerShadowVisible: false,
+          headerTitleAlign: 'center',
+          headerTitle: () => (
+            <Text style={textStyle}>{t('home.send.label')}</Text>
+          ),
+          headerLeft: () => (
+            <Pressable style={buttonStyle} onPress={goBack}>
+              <FontAwesome5 name="chevron-left" color={textColor} size={24} />
+            </Pressable>
+          ),
+        }}
+        name="SendTip"
+        component={SendTipScreen}
       />
       <Stack.Screen
         options={{ headerShown: false }}
@@ -40,4 +92,4 @@ const SendStackStack = () => {
   );
 };
 
-export default SendStackStack;
+export default SendStack;

f0f4e474aed637eaaf99c2b914c417ac3e0aac04
juam <juanmanso@gmail.com>
2023-06-09 15:18:53 +0200

    feat: init quai pay input display

--- src/shared/components/index.tsx
+++ src/shared/components/index.tsx
@@ -1,3 +1,4 @@
 export { QuaiPayContent } from './QuaiPayContent';
+export { QuaiPayInputDisplay } from './QuaiPayInputDisplay';
 export { QuaiPayKeyboard } from './QuaiPayKeyboard';
 export { QuaiPayText } from './QuaiPayText';

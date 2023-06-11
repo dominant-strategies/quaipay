0586b83207033053d9317706269e83a3cbdce8a4
juam <juanmanso@gmail.com>
2023-06-11 18:00:16 +0200

    feat: add insets control via props

--- src/shared/components/QuaiPayContent.tsx
+++ src/shared/components/QuaiPayContent.tsx
@@ -1,17 +1,36 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import { useThemedStyle } from '../hooks/useThemedStyle';
 import { Theme } from '../types';
 
 interface QuaiPayContentProps {
   children: React.ReactNode;
+  noInsetBottom: boolean;
 }
 
-export const QuaiPayContent: React.FC<QuaiPayContentProps> = ({ children }) => {
+export const QuaiPayContent: React.FC<QuaiPayContentProps> = ({
+  children,
+  noInsetBottom = false,
+}) => {
+  const insets = useSafeAreaInsets();
   const styles = useThemedStyle(themedStyle);
 
-  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          paddingTop: insets.top,
+          paddingBottom: noInsetBottom ? 0 : insets.bottom,
+        },
+      ]}
+    >
+      {children}
+    </View>
+  );
 };
 
 const themedStyle = (theme: Theme) =>

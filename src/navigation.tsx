68cf48024035185c2b35d1728e184c24860a3321
juam <juanmanso@gmail.com>
2023-06-11 20:35:10 +0200

    feat: copy RootStackParamList definition to navigation file

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import {
   NavigationContainer,
+  NavigatorScreenParams,
   createNavigationContainerRef,
 } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import { useTheme } from './shared/context/themeContext';
+import { ReceiveStackParamList } from './main/home/receive/ReceiveStack';
+import { SendStackParamList } from './main/home/send/SendStack';
 
-export type RootStackParamList = {};
+export type RootStackParamList = {
+  Onboarding: undefined;
+  Main: undefined;
+  ReceiveStack: NavigatorScreenParams<ReceiveStackParamList>;
+  SendStack: NavigatorScreenParams<SendStackParamList>;
+};
 
 const navigationRef = createNavigationContainerRef<RootStackParamList>();
 

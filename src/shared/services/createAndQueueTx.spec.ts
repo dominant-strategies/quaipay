1c77c172e818c9e6d85adf99b80d6db78824207e
obasilakis <vasileios@keyko.io>
2023-06-12 17:45:09 +0300

    feat: add functions to create and queue txs

---
+++ src/shared/services/createAndQueueTx.spec.ts
@@ -0,0 +1,66 @@
+import { queueTransaction, TxData } from './createAndQueueTx';
+import { quais } from 'quais';
+
+jest.mock('@react-native-async-storage/async-storage', () => {
+  return {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+  };
+});
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+describe('queueTransaction', () => {
+  describe('when there are pending txs queued', function () {
+    it('returns true when tx was successfully saved to AsyncStorage', async () => {
+      const tx1: TxData = { to: '0x123', value: quais.utils.parseEther('1') };
+      const stringifiedTx1 = JSON.stringify([tx1]);
+      const tx2: TxData = { to: '0x234', value: quais.utils.parseEther('2') };
+      const stringifiedTxs = JSON.stringify([tx1, tx2]);
+      // @ts-ignore
+      AsyncStorage.getItem.mockImplementation(() => stringifiedTx1);
+      const setItemSpy = jest.spyOn(AsyncStorage, 'setItem');
+
+      const queueTxPromise = queueTransaction(tx2);
+
+      await expect(queueTxPromise).resolves.toBe(true);
+      expect(setItemSpy).toHaveBeenCalledWith(
+        '@pending_transactions',
+        stringifiedTxs,
+      );
+    });
+  });
+
+  describe('when there are no pending txs', function () {
+    it('returns true when tx was successfully saved to AsyncStorage', async () => {
+      const tx: TxData = { to: '0x123', value: quais.utils.parseEther('1') };
+      const stringifiedTx = JSON.stringify([tx]);
+
+      // @ts-ignore
+      AsyncStorage.getItem.mockImplementation(() => undefined);
+      const setItemSpy = jest.spyOn(AsyncStorage, 'setItem');
+
+      const queueTxPromise = queueTransaction(tx);
+
+      await expect(queueTxPromise).resolves.toBe(true);
+      expect(setItemSpy).toHaveBeenCalledWith(
+        '@pending_transactions',
+        stringifiedTx,
+      );
+    });
+  });
+
+  it('returns false when tx could not be saved to AsyncStorage', async () => {
+    const tx: TxData = { to: '0x123', value: quais.utils.parseEther('1') };
+    // @ts-ignore
+    AsyncStorage.getItem.mockImplementation(() => undefined);
+    // @ts-ignore
+    AsyncStorage.setItem.mockImplementation(() => {
+      throw new Error();
+    });
+
+    const queueTxPromise = queueTransaction(tx);
+
+    await expect(queueTxPromise).resolves.toBe(false);
+  });
+});
---
+++ src/shared/services/createAndQueueTx.ts
@@ -0,0 +1,51 @@
+import { quais } from 'quais';
+import { getQuaisProvider } from './getQuaisProvider';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { BigNumber } from '@quais/bignumber';
+
+export type TxData = {
+  to: string;
+  value: BigNumber;
+  hash?: string;
+};
+
+export const createAndQueueTx = async (to: string, amount: string) => {
+  const provider = getQuaisProvider();
+  await provider.ready;
+
+  const valueToTransferInWei = quais.utils.parseEther(amount);
+
+  const tx: TxData = { to, value: valueToTransferInWei };
+
+  let txQueued = await queueTransaction(tx);
+  while (!txQueued) {
+    txQueued = await queueTransaction(tx);
+  }
+};
+
+export const queueTransaction = async (
+  transactionData: TxData,
+): Promise<boolean> => {
+  try {
+    const stingifiedPendingTransactions = await AsyncStorage.getItem(
+      '@pending_transactions',
+    );
+    let pendingTransactions: TxData[] = [];
+
+    if (stingifiedPendingTransactions) {
+      pendingTransactions = JSON.parse(stingifiedPendingTransactions);
+      pendingTransactions.push(transactionData);
+    } else {
+      pendingTransactions.push(transactionData);
+    }
+
+    await AsyncStorage.setItem(
+      '@pending_transactions',
+      JSON.stringify(pendingTransactions),
+    );
+    return true;
+  } catch (e) {
+    console.log('Error storing transaction: ', e);
+    return false;
+  }
+};

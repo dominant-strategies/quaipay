c088731b62c71abe1a5ce0f3a1c01e9d1d6ba16e
juam <juanmanso@gmail.com>
2023-06-13 04:25:04 +0200

    feat: add export button with dummy callback

--- src/main/settings/SettingScreen.tsx
+++ src/main/settings/SettingScreen.tsx
@@ -1,23 +1,44 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
 
-import { QuaiPayContent } from 'src/shared/components';
+import { QuaiPayContent, QuaiPayText } from 'src/shared/components';
 import { MainTabStackScreenProps } from '../MainStack';
+import { useThemedStyle } from 'src/shared/hooks/useThemedStyle';
+import { Theme } from 'src/shared/types';
 
-const SettingScreen: React.FC<MainTabStackScreenProps<'Setting'>> = () => {
+const SettingScreen: React.FC<MainTabStackScreenProps<'Setting'>> = ({}) => {
+  const styles = useThemedStyle(themedStyles);
+
+  const goToExport = () => true;
+
   return (
     <QuaiPayContent title={'Settings'} navButton={false}>
-      <View style={styles.container} />
+      <View style={styles.container}>
+        <Pressable
+          onPress={goToExport}
+          style={({ pressed }) => [
+            styles.placeholderBtn,
+            pressed && { opacity: 0.5 },
+          ]}
+        >
+          <QuaiPayText type="H3">Export Account</QuaiPayText>
+        </Pressable>
+      </View>
     </QuaiPayContent>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const themedStyles = (theme: Theme) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    placeholderBtn: {
+      padding: 10,
+      backgroundColor: theme.normal,
+    },
+  });
 
 export default SettingScreen;

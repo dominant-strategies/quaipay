82e11c90bbeaf7828652450c766c01de002c94fa
juam <juanmanso@gmail.com>
2023-06-13 05:26:52 +0200

    feat: add basic layout of screen

--- src/main/settings/export/ExportLandingScreen.tsx
+++ src/main/settings/export/ExportLandingScreen.tsx
@@ -1,16 +1,57 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
 
-import { QuaiPayContent } from 'src/shared/components';
+import { QuaiPayContent, QuaiPayText } from 'src/shared/components';
+import { Theme } from 'src/shared/types';
+import { useThemedStyle } from 'src/shared/hooks/useThemedStyle';
 
 import { ExportStackScreenProps } from './ExportStack';
 
+// TODO: move copy to i18n
+// TODO: tweak UI to match L&F
 export const ExportLandingScreen: React.FC<
   ExportStackScreenProps<'ExportLanding'>
 > = ({}) => {
+  const styles = useThemedStyle(themedStyle);
   return (
     <QuaiPayContent>
-      <View />
+      <View style={styles.textContainer}>
+        <QuaiPayText type="H1">Setup Account Recovery</QuaiPayText>
+        <QuaiPayText type="H3">Description</QuaiPayText>
+      </View>
+      <Pressable
+        style={({ pressed }) => [styles.card, pressed && { opacity: 0.5 }]}
+      >
+        <QuaiPayText>Set up Seed Phrase</QuaiPayText>
+      </Pressable>
+      <Pressable
+        style={({ pressed }) => [styles.card, pressed && { opacity: 0.5 }]}
+      >
+        <QuaiPayText>Account Recovery QR Code</QuaiPayText>
+      </Pressable>
+      <View style={styles.separator} />
+      <QuaiPayText style={styles.learnMore}>Learn more</QuaiPayText>
     </QuaiPayContent>
   );
 };
+
+const themedStyle = (theme: Theme) =>
+  StyleSheet.create({
+    textContainer: {
+      flex: 1,
+      alignItems: 'center',
+      marginBottom: 20,
+    },
+    card: {
+      backgroundColor: theme.secondary,
+      marginBottom: 12,
+      padding: 10,
+      marginHorizontal: 32,
+    },
+    learnMore: {
+      marginBottom: 70,
+    },
+    separator: {
+      flex: 1,
+    },
+  });

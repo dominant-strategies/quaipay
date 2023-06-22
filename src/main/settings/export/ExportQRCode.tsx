e9268379579847904340a22b065eec70fe949476
juam <juanmanso@gmail.com>
2023-06-23 01:47:25 +0200

    fix: tweak UI of the screen

--- src/main/settings/export/ExportQRCode.tsx
+++ src/main/settings/export/ExportQRCode.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Pressable, StyleSheet, View } from 'react-native';
+import { useTranslation } from 'react-i18next';
 
 import {
   QuaiPayContent,
@@ -29,21 +30,19 @@ export const ExportQRCodeScreen: React.FC<
   return (
     <QuaiPayContent>
       <View style={styles.separator} />
-      <View style={styles.flexContainer}>
-        <View style={styles.cardContainer}>
-          <QuaiPayText type="H1">{t('title')}</QuaiPayText>
-          <QuaiPayText type="paragraph" themeColor="secondary">
-            {t('description')}
-          </QuaiPayText>
+      <View style={styles.cardContainer}>
+        <QuaiPayText type="H1">{t('title')}</QuaiPayText>
+        <QuaiPayText type="paragraph" themeColor="secondary">
+          {t('description')}
+        </QuaiPayText>
         <QuaiPayQRCode
           containerStyle={styles.qrCodeContainer}
           value={mnemonicPhrase}
         />
-        </View>
-        <QuaiPayText style={styles.underline} themeColor="secondary">
-          {t('learnMore')}
-        </QuaiPayText>
       </View>
+      <QuaiPayText style={styles.underline} themeColor="secondary">
+        {t('learnMore')}
+      </QuaiPayText>
       <View style={styles.separator} />
       <Pressable
         onPress={goToSettings}
@@ -51,15 +50,13 @@ export const ExportQRCodeScreen: React.FC<
       >
         <QuaiPayText type="H3">{t('complete')}</QuaiPayText>
       </Pressable>
+      <View style={styles.separator} />
     </QuaiPayContent>
   );
 };
 
 const themedStyle = (theme: Theme) =>
   StyleSheet.create({
-    flexContainer: {
-      flex: 1,
-    },
     cardContainer: {
       alignItems: 'center',
       marginHorizontal: 16,
@@ -72,7 +69,6 @@ const themedStyle = (theme: Theme) =>
       borderColor: theme.border,
     },
     button: {
-      marginBottom: 70,
       padding: 16,
       marginHorizontal: 30,
       backgroundColor: theme.surface,

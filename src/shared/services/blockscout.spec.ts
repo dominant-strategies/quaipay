51465c56ab92d9592880c42555c5c2b1de6bb9be
hsyndeniz <huseyin.deniz1@hotmail.com>
2023-06-23 15:57:23 +0300

    update tests for tx history

---
+++ src/shared/services/blockscout.spec.ts
@@ -0,0 +1,27 @@
+import { getAccountTransactions } from './blockscout';
+
+describe('getAccountTransactions', () => {
+  it('should return a Promise that resolves to an array of transactions', async () => {
+    const transactions = await getAccountTransactions(
+      '0x2f7662cD8E784750E116E44a536278d2b429167E',
+    );
+    expect(Array.isArray(transactions.result)).toBe(true);
+    expect(transactions.status).toBe('1');
+    expect(transactions.message).toBe('OK');
+  });
+
+  it('should accept optional parameters', async () => {
+    const transactions = await getAccountTransactions(
+      '0x2f7662cD8E784750E116E44a536278d2b429167E',
+      'asc',
+      1,
+      10,
+      'to',
+      1620000000,
+      Date.now(),
+    );
+    expect(Array.isArray(transactions.result)).toBe(true);
+    expect(transactions.status).toBe('1');
+    expect(transactions.message).toBe('OK');
+  });
+});

9d8060ac805f52a578fda7b4a733d0c69c3e0637
obasilakis <vasileios@keyko.io>
2023-06-23 13:26:11 +0300

    chore: cast the type

--- src/onboarding/services/retrieveEntropy.ts
+++ src/onboarding/services/retrieveEntropy.ts
@@ -1,8 +1,10 @@
 import { retrieveStoredItem } from 'src/shared/services/keychain';
 
 export async function retrieveEntropy() {
-  const retrievedEntropy = await retrieveStoredItem('entropy');
-  // ts-ignore
+  // TODO: remove type casting
+  const retrievedEntropy = (await retrieveStoredItem('entropy')) as
+    | string
+    | false;
   if (retrievedEntropy === false) {
     throw new Error('Entropy not found in keychain');
   }

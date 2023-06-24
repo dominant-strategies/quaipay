828be6afd6896b05dfc9f8817613b341398732e5
hsyndeniz <huseyin.deniz1@hotmail.com>
2023-06-25 01:28:32 +0300

    update tests for tx history

--- src/shared/services/blockscout.spec.ts
+++ src/shared/services/blockscout.spec.ts
@@ -2,28 +2,28 @@ import { getAccountTransactions } from './blockscout';
 
 describe('getAccountTransactions', () => {
   it('should return a Promise that resolves to an array of transactions', async () => {
-    const transactions = await getAccountTransactions(
-      '0x2f7662cD8E784750E116E44a536278d2b429167E',
-    );
+    const transactions = await getAccountTransactions({
+      address: '0x2f7662cD8E784750E116E44a536278d2b429167E',
+    });
+    const regex = new RegExp(/^[01]$/);
+    expect(regex.test(transactions.status)).toBe(true);
     expect(Array.isArray(transactions.result)).toBe(true);
-    expect(transactions.status).toBe('1');
-    expect(transactions.message).toBe('OK');
   });
 
   it('should accept optional parameters', async () => {
-    const transactions = await getAccountTransactions(
-      '0x2f7662cD8E784750E116E44a536278d2b429167E',
-      'asc',
-      1,
-      10,
-      1620000000,
-      Date.now(),
-      'to',
-      50000000000000000,
-      1000000000000000000,
-    );
+    const transactions = await getAccountTransactions({
+      address: '0x2f7662cD8E784750E116E44a536278d2b429167E',
+      sort: 'desc',
+      page: 1,
+      offset: 10,
+      start_timestamp: 1620000000,
+      end_timestamp: Date.now(),
+      filter_by: 'to',
+      min_amount: 50000000000000000,
+      max_amount: 1000000000000000000,
+    });
+    const regex = new RegExp(/^[01]$/);
+    expect(regex.test(transactions.status)).toBe(true);
     expect(Array.isArray(transactions.result)).toBe(true);
-    expect(transactions.status).toBe('1');
-    expect(transactions.message).toBe('OK');
   });
 });

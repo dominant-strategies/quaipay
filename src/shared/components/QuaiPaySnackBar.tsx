82d5bda7fa5872a112d41ff96b355c1d1f228930
juam <juanmanso@gmail.com>
2023-06-25 23:33:33 +0200

    feat: add basic styling and animations

--- src/shared/components/QuaiPaySnackBar.tsx
+++ src/shared/components/QuaiPaySnackBar.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import Animated, {
+  Easing,
+  FadeInDown,
+  FadeOutDown,
+  Layout,
+} from 'react-native-reanimated';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 import { QuaiPayText } from './QuaiPayText';
+
+const Z_INDEX_SNACKBAR = 10;
 
 interface QuaiPaySnackBarProps {
   message: string;
@@ -9,15 +19,32 @@ interface QuaiPaySnackBarProps {
 export const QuaiPaySnackBar: React.FC<QuaiPaySnackBarProps> = ({
   message,
 }) => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View style={styles.container}>
-      <QuaiPayText>{message}</QuaiPayText>
+    <View style={[styles.container, { bottom: insets.bottom + 16 }]}>
+      <Animated.View
+        entering={FadeInDown.delay(300)}
+        exiting={FadeOutDown}
+        layout={Layout.easing(Easing.linear)}
+        style={styles.snackBar}
+      >
+        <QuaiPayText>{message}</QuaiPayText>
+      </Animated.View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '100%',
+    position: 'absolute',
+  },
+  snackBar: {
+    backgroundColor: 'red',
+    paddingVertical: 16,
+    borderRadius: 8,
+    marginHorizontal: 20,
+    zIndex: 1000 + Z_INDEX_SNACKBAR,
   },
 });

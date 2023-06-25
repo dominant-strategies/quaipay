eceadc634baea18401bbd23d2ba4cf7d3edf357c
juam <juanmanso@gmail.com>
2023-06-25 17:43:55 +0200

    feat: add default styling

--- src/shared/components/QuaiPayButton.tsx
+++ src/shared/components/QuaiPayButton.tsx
@@ -6,23 +6,47 @@ import {
   StyleSheet,
   ViewStyle,
 } from 'react-native';
+import { QuaiPayText } from './QuaiPayText';
+
+import { useTheme } from '../context/themeContext';
+import { Theme } from '../types';
+import { styledColors } from '../styles';
+
+type QuaiPayButtonType = 'default';
+
+interface QuaiPayButtonStyle {
+  backgroundColor: string;
+  textColor: string;
+}
 
 interface QuaiPayButtonProps extends Omit<PressableProps, 'style'> {
   style?: StyleProp<ViewStyle>;
   title: string;
+  type?: QuaiPayButtonType;
 }
 
 export const QuaiPayButton: React.FC<QuaiPayButtonProps> = ({
   title,
   style,
+  type = 'default',
   ...props
 }) => {
+  const { theme } = useTheme();
+  const buttonStyles = buttonStylesByType[type](theme);
   return (
     <Pressable
       {...props}
-      style={({ pressed }) => [pressed && styles.opacity50, style]}
+      style={({ pressed }) => [
+        {
+          backgroundColor: buttonStyles.backgroundColor,
+        },
+        pressed && styles.opacity50,
+        style,
+      ]}
     >
-      {title}
+      <QuaiPayText style={{ color: buttonStyles.textColor }}>
+        {title}
+      </QuaiPayText>
     </Pressable>
   );
 };
@@ -32,3 +56,13 @@ const styles = StyleSheet.create({
     opacity: 0.5,
   },
 });
+
+const buttonStylesByType: Record<
+  QuaiPayButtonType,
+  (theme: Theme) => QuaiPayButtonStyle
+> = {
+  default: (theme: Theme) => ({
+    backgroundColor: theme.normal,
+    textColor: styledColors.white,
+  }),
+};

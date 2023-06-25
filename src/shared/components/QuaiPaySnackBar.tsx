0ad4318daf9480c752bb6adb5d959e121ee47351
juam <juanmanso@gmail.com>
2023-06-26 01:44:44 +0200

    feat: add on end animations

--- src/shared/components/QuaiPaySnackBar.tsx
+++ src/shared/components/QuaiPaySnackBar.tsx
@@ -1,21 +1,26 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
 import Animated, {
   Easing,
   FadeInDown,
   FadeOutDown,
   Layout,
   interpolate,
+  runOnJS,
   useAnimatedGestureHandler,
   useAnimatedStyle,
   useSharedValue,
+  withSpring,
 } from 'react-native-reanimated';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { PanGestureHandler } from 'react-native-gesture-handler';
 
 import { QuaiPayText } from './QuaiPayText';
 
+const SWIPE_THRESHOLD = 150;
 const Z_INDEX_SNACKBAR = 10;
+
+const SCREEN_WIDTH = Dimensions.get('screen').width;
 
 interface QuaiPaySnackBarProps {
   message: string;
@@ -26,15 +31,24 @@ export const QuaiPaySnackBar: React.FC<QuaiPaySnackBarProps> = ({
 }) => {
   const insets = useSafeAreaInsets();
   const translateX = useSharedValue(0);
+  const close = () => false;
 
   const gestureHandler = useAnimatedGestureHandler({
     onActive: event => {
       translateX.value = event.translationX;
     },
+    onEnd: event => {
+      if (event.translationX > SWIPE_THRESHOLD) {
+        translateX.value = withSpring(SCREEN_WIDTH);
+        runOnJS(close)();
+      } else {
+        translateX.value = withSpring(0);
+      }
+    },
   });
 
   const animatedStyle = useAnimatedStyle(() => ({
-    opacity: interpolate(translateX.value, [0, 150], [1, 0.2]),
+    opacity: interpolate(translateX.value, [0, SWIPE_THRESHOLD], [1, 0.2]),
     transform: [
       {
         translateX: translateX.value > 0 ? translateX.value : 0,

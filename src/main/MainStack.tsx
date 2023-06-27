3666df752cb9ea6a602606c7b51b31c98a11c6c6
juam <juanmanso@gmail.com>
2023-06-27 22:22:07 +0200

    chore: clean up unused screens on main tab stack

--- src/main/MainStack.tsx
+++ src/main/MainStack.tsx
@@ -7,17 +7,13 @@ import {
 } from '@react-navigation/bottom-tabs';
 import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { default as Icon } from 'react-native-vector-icons/FontAwesome';
 import { default as AntIcon } from 'react-native-vector-icons/AntDesign';
-import { default as MaterialIcon } from 'react-native-vector-icons/MaterialIcons';
 
 import { QuaiPayContent } from 'src/shared/components';
 import { RootStackScreenProps } from 'src/shared/navigation';
 import { useTheme } from 'src/shared/context/themeContext';
 import { styledColors } from 'src/shared/styles';
 
-import ExchangeScreen from './exchange/ExchangeScreen';
-import EarnScreen from './earn/EarnScreen';
 import SettingsScreen from './settings/SettingsScreen';
 import HomeScreen from './home/HomeScreen';
 import WalletScreen from './wallet/WalletScreen';
@@ -93,29 +89,11 @@ const MainStack: React.FC<RootStackScreenProps<'Main'>> = ({ route }) => {
           }}
         />
         <Tab.Screen
-          name="Exchange"
-          component={ExchangeScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <MaterialIcon name="swap-horiz" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
           name="Home"
           component={HomeScreen}
           options={{
             tabBarIcon: ({ color, size }) => (
               <AntIcon name="home" size={size} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Earn"
-          component={EarnScreen}
-          options={{
-            tabBarIcon: ({ color, size }) => (
-              <Icon name="dollar" size={size} color={color} />
             ),
           }}
         />

e713ec4c108ff730afbb6e0870bb9ccc66359f78
obasilakis <vasileios@keyko.io>
2023-07-04 12:13:19 +0300

    fix: remove second JSON.parse

--- src/shared/services/retrieveWallet.ts
+++ src/shared/services/retrieveWallet.ts
@@ -3,10 +3,6 @@ import { keychainKeys } from '../constants/keychainKeys';
 import { Wallet } from '../types/Wallet';
 import { Zone } from 'src/shared/types';
 
-export const getZone = () => {};
-// TODO: use location when we have a mapping from location to zone
-// const location = await retrieveStoredItem('location');
-
 export const retrieveWallet = async (): Promise<
   Record<Zone, Wallet> | undefined
 > => {
@@ -14,8 +10,6 @@ export const retrieveWallet = async (): Promise<
   if (!retrievedWallet) {
     throw new Error('Wallet not found');
   }
-  // const zone = getZone(location);
-  // const wallet = JSON.parse(retrievedWallet)[zone];
-  const wallet = JSON.parse(retrievedWallet)['zone-1-0'];
-  return JSON.parse(wallet);
+
+  return JSON.parse(retrievedWallet);
 };

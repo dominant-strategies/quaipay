59cdc24bcd669e2fd232a45d6b470341830328b7
obasilakis <vasileios@keyko.io>
2023-07-05 13:36:17 +0300

    feat: top part of settings landing page

--- src/main/settings/SettingsScreen.tsx
+++ src/main/settings/SettingsScreen.tsx
@@ -1,31 +1,43 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Dimensions, StyleSheet, View } from 'react-native';
 
 import { RootNavigator } from 'src/shared/navigation/utils';
-import { QuaiPayButton, QuaiPayContent } from 'src/shared/components';
 
 import { MainTabStackScreenProps } from '../MainStack';
+import { Theme } from 'src/shared/types';
+import AvatarPlaceHolder from 'src/shared/assets/avatarPlaceholder.svg';
+import { useThemedStyle } from 'src/shared/hooks';
 
 const SettingsScreen: React.FC<MainTabStackScreenProps<'Settings'>> = () => {
+  const styles = useThemedStyle(themedStyle);
+
   const goToExport = () =>
     RootNavigator.navigate('ExportStack', { screen: 'ExportLanding' });
 
   return (
-    <QuaiPayContent title={'Settings'} noNavButton>
-      <View style={styles.container}>
-        <QuaiPayButton onPress={goToExport} title="Export Account" />
-      </View>
-    </QuaiPayContent>
+    <View style={styles.top}>
+      <AvatarPlaceHolder style={styles.avatar} width={96} height={96} />
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 20,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const themedStyle = (theme: Theme) =>
+  StyleSheet.create({
+    top: {
+      backgroundColor: theme.background,
+      height: Dimensions.get('window').height * 0.2,
+      borderBottomColor: theme.border,
+      borderBottomWidth: 1,
+      position: 'relative',
+    },
+    avatar: {
+      borderColor: theme.surface,
+      borderWidth: 6,
+      borderRadius: 48,
+      bottom: -30,
+      left: Dimensions.get('window').width / 2 - 48,
+      position: 'absolute',
+    },
+  });
 
 export default SettingsScreen;

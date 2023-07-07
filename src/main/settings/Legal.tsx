097b1a642afb417fda79d426b23157c147d74d35
obasilakis <vasileios@keyko.io>
2023-07-07 23:30:36 +0300

    feat: add legal screen

--- src/main/settings/Legal.tsx
+++ src/main/settings/Legal.tsx
@@ -1,11 +1,88 @@
 import React from 'react';
-import { Text } from 'react-native';
-import { QuaiPayContent } from 'src/shared/components';
+import { ScrollView, StyleSheet } from 'react-native';
+import { QuaiPayContent, QuaiPayText } from 'src/shared/components';
+import { useTranslation } from 'react-i18next';
+import { useThemedStyle } from 'src/shared/hooks';
+import { Theme } from 'src/shared/types';
 
 export const Legal = () => {
+  const { t } = useTranslation('translation', {
+    keyPrefix: 'settings.legal',
+  });
+  const styles = useThemedStyle(themedStyle);
+
   return (
     <QuaiPayContent>
-      <Text>Legal</Text>
+      <ScrollView contentContainerStyle={styles.container}>
+        <QuaiPayText type="H3">{t('title')}</QuaiPayText>
+        <QuaiPayText style={[styles.descriptionText, styles.secondary]}>
+          {t('description')}
+        </QuaiPayText>
+        <QuaiPayText style={styles.secondary} type="bold">
+          {t('firstPoint')}
+        </QuaiPayText>
+        <QuaiPayText style={[styles.descriptionText, styles.secondary]}>
+          {t('firstPointDescription')}
+        </QuaiPayText>
+        <QuaiPayText style={styles.secondary} type="bold">
+          {t('secondPoint')}
+        </QuaiPayText>
+        <QuaiPayText style={[styles.descriptionText, styles.secondary]}>
+          {t('secondPointDescription')}
+        </QuaiPayText>
+        <QuaiPayText style={styles.secondary} type="bold">
+          {t('thirdPoint')}
+        </QuaiPayText>
+        <QuaiPayText style={[styles.descriptionText, styles.secondary]}>
+          {t('thirdPointDescription')}
+        </QuaiPayText>
+        <QuaiPayText style={styles.secondary} type="bold">
+          {t('fourthPoint')}
+        </QuaiPayText>
+        <QuaiPayText style={[styles.descriptionText, styles.secondary]}>
+          {t('fourthPointDescription')}
+        </QuaiPayText>
+        <QuaiPayText style={styles.secondary} type="bold">
+          {t('fifthPoint')}
+        </QuaiPayText>
+        <QuaiPayText style={[styles.descriptionText, styles.secondary]}>
+          {t('fifthPointDescription')}
+        </QuaiPayText>
+        <QuaiPayText style={styles.secondary} type="bold">
+          {t('sixthPoint')}
+        </QuaiPayText>
+        <QuaiPayText style={[styles.descriptionText, styles.secondary]}>
+          {t('sixthPointDescription')}
+        </QuaiPayText>
+        <QuaiPayText style={styles.secondary} type="bold">
+          {t('seventhPoint')}
+        </QuaiPayText>
+        <QuaiPayText style={[styles.descriptionText, styles.secondary]}>
+          {t('seventhPointDescription')}
+        </QuaiPayText>
+      </ScrollView>
     </QuaiPayContent>
   );
 };
+
+const themedStyle = (theme: Theme) =>
+  StyleSheet.create({
+    container: {
+      alignItems: 'flex-start',
+      padding: 16,
+    },
+    descriptionText: {
+      textAlign: 'left',
+      marginBottom: 16,
+    },
+    secondary: {
+      color: theme.secondary,
+    },
+    button: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderColor: theme.normal,
+      borderRadius: 4,
+      borderWidth: 1,
+    },
+  });

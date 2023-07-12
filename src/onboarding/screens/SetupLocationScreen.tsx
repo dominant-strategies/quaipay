833add358beb35c1890b541c68a7d6fbab9e3304
juam <juanmanso@gmail.com>
2023-07-12 14:19:18 +0200

    chore: remove old implementation of location setup

--- src/onboarding/screens/SetupLocationScreen.tsx
+++ src/onboarding/screens/SetupLocationScreen.tsx
@@ -1,10 +1,5 @@
-import React, { useCallback, useState } from 'react';
-import Geolocation, {
-  GeoOptions,
-  GeoPosition,
-} from 'react-native-geolocation-service';
-import { PermissionsAndroid, Platform, StyleSheet, View } from 'react-native';
-import { PERMISSIONS, request } from 'react-native-permissions';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useTranslation } from 'react-i18next';
 
@@ -17,14 +12,9 @@ import {
   QuaiPayLoader,
   QuaiPayText,
 } from 'src/shared/components';
+import { Zone } from 'src/shared/types';
 
 import { OnboardingStackScreenProps } from '../OnboardingStack';
-
-async function getPosition(options?: GeoOptions): Promise<GeoPosition> {
-  return new Promise((resolve, reject) =>
-    Geolocation.getCurrentPosition(resolve, reject, options),
-  );
-}
 
 export const SetupLocationScreen: React.FC<
   OnboardingStackScreenProps<'SetupLocation'>
@@ -35,60 +25,18 @@ export const SetupLocationScreen: React.FC<
   const { t: tCommon } = useTranslation('translation', { keyPrefix: 'common' });
   const [loading, setGettingLocation] = useState(false);
 
-  const requestLocationPermission = async () => {
-    if (Platform.OS === 'ios') {
-      const permission =
-        parseInt(Platform.Version, 10) < 13
-          ? PERMISSIONS.IOS.LOCATION_ALWAYS
-          : PERMISSIONS.IOS.LOCATION_WHEN_IN_USE;
-      return await request(permission);
-    } else if (Platform.OS === 'android') {
-      try {
-        const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-          {
-            title: 'Geolocation Permission',
-            message: 'Can we access your location?',
-            buttonNeutral: 'Ask Me Later',
-            buttonNegative: 'Cancel',
-            buttonPositive: 'OK',
-          },
-        );
-        console.log('granted', granted);
-        if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-          console.log('You can use Geolocation');
-          return true;
-        } else {
-          console.log('You cannot use Geolocation');
-          return false;
-        }
-      } catch (err) {
-        console.log(err);
-        return false;
-      }
-    }
-  };
-
-  const getLocation = useCallback(async () => {
+  const setupLocation = async () => {
     setGettingLocation(true);
-    const result = await requestLocationPermission();
-    if (result) {
-      const position = await getPosition({
-        enableHighAccuracy: true,
-        timeout: 15000,
-        maximumAge: 10000,
-      });
-      const { latitude, longitude } = position.coords;
-      await storeItem({
-        key: keychainKeys.location,
-        value: JSON.stringify({ latitude, longitude }),
-      });
+    await storeItem({
+      key: keychainKeys.location,
+      // TODO: get from selector
+      value: JSON.stringify({ zone: Zone['zone-0-0'] }),
+    });
 
-      setGettingLocation(false);
-      await AsyncStorage.setItem('onboarded', 'true');
-      RootNavigator.goHome();
-    }
-  }, []);
+    setGettingLocation(false);
+    await AsyncStorage.setItem('onboarded', 'true');
+    RootNavigator.goHome();
+  };
 
   return loading ? (
     <QuaiPayLoader text={t('loading')} />
@@ -99,7 +47,7 @@ export const SetupLocationScreen: React.FC<
       <View style={styles.separator} />
       <QuaiPayButton
         title={tCommon('continue')}
-        onPress={getLocation}
+        onPress={setupLocation}
         style={styles.continueButton}
       />
     </QuaiPayContent>

87bdbe5f636b5a878a57a563c68e001bf727106b
juam <juanmanso@gmail.com>
2023-07-12 17:26:29 +0200

    feat: add location picker

--- src/onboarding/screens/SetupLocationScreen.tsx
+++ src/onboarding/screens/SetupLocationScreen.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useTranslation } from 'react-i18next';
+import { Picker } from '@react-native-picker/picker';
 
 import { storeItem } from 'src/shared/services/keychain';
 import { keychainKeys } from 'src/shared/constants/keychainKeys';
@@ -13,8 +14,13 @@ import {
   QuaiPayText,
 } from 'src/shared/components';
 import { Zone } from 'src/shared/types';
+import { zoneRegionMap } from 'src/shared/assets/regions';
+import { typography } from 'src/shared/styles';
+import { useTheme } from 'src/shared/context/themeContext';
 
 import { OnboardingStackScreenProps } from '../OnboardingStack';
+
+const INITIAL_ZONE = Zone['zone-0-0'];
 
 export const SetupLocationScreen: React.FC<
   OnboardingStackScreenProps<'SetupLocation'>
@@ -22,15 +28,17 @@ export const SetupLocationScreen: React.FC<
   const { t } = useTranslation('translation', {
     keyPrefix: 'onboarding.setup.location',
   });
+  const { theme } = useTheme();
   const { t: tCommon } = useTranslation('translation', { keyPrefix: 'common' });
   const [loading, setGettingLocation] = useState(false);
+  const [selectedZone, setSelectedZone] = useState(INITIAL_ZONE);
 
   const setupLocation = async () => {
     setGettingLocation(true);
     await storeItem({
       key: keychainKeys.location,
       // TODO: get from selector
-      value: JSON.stringify({ zone: Zone['zone-0-0'] }),
+      value: JSON.stringify({ zone: selectedZone }),
     });
 
     setGettingLocation(false);
@@ -44,6 +52,23 @@ export const SetupLocationScreen: React.FC<
     <QuaiPayContent hasBackgroundVariant containerStyle={styles.mainContainer}>
       <QuaiPayText type="H1">{t('title')}</QuaiPayText>
       <QuaiPayText style={styles.description}>{t('description')}</QuaiPayText>
+      <View style={styles.separator} />
+      <Picker
+        mode="dropdown"
+        selectedValue={selectedZone}
+        itemStyle={{ ...styles.pickerItem, color: theme.primary }}
+        onValueChange={item => setSelectedZone(item)}
+      >
+        {Object.values(Zone).map((item, idx) => (
+          <Picker.Item
+            key={idx}
+            label={zoneRegionMap[item]}
+            value={item}
+            color={item === selectedZone ? theme.normal : theme.primary}
+            style={styles.pickerItem}
+          />
+        ))}
+      </Picker>
       <View style={styles.separator} />
       <QuaiPayButton
         title={tCommon('continue')}
@@ -67,5 +92,8 @@ const styles = StyleSheet.create({
   },
   separator: {
     flex: 1,
+  },
+  pickerItem: {
+    ...typography.H3,
   },
 });
--- src/shared/assets/regions/index.ts
+++ src/shared/assets/regions/index.ts
@@ -1,5 +1,7 @@
 import { ImageSourcePropType } from 'react-native';
 
+import { Zone } from 'src/shared/types';
+
 export enum Region {
   EAST_ASIA_AND_SOUTHEAST_ASIA = 'East Asia and Southeast Asia',
   INDIA = 'India',
@@ -11,6 +13,18 @@ export enum Region {
   SOUTH_AMERICA = 'South America',
   WEST_EUROPE = 'West Europe',
 }
+
+export const zoneRegionMap: Record<Zone, Region> = {
+  'zone-0-0': Region.EAST_ASIA_AND_SOUTHEAST_ASIA,
+  'zone-0-1': Region.INDIA,
+  'zone-0-2': Region.MIDDLE_EAST,
+  'zone-1-0': Region.NA_EAST,
+  'zone-1-1': Region.NA_WEST,
+  'zone-1-2': Region.OCEANIA,
+  'zone-2-0': Region.RUSSIA,
+  'zone-2-1': Region.SOUTH_AMERICA,
+  'zone-2-2': Region.WEST_EUROPE,
+};
 
 export const regionImgs: Record<Region, ImageSourcePropType> = {
   [Region.EAST_ASIA_AND_SOUTHEAST_ASIA]: require('./East_Asia_And_Southeast_Asia.png'),

6b0e596b0c1b89dbe08136ba6b112b6beb6bdebe
juam <juanmanso@gmail.com>
2023-07-21 16:04:36 +0100

    feat: add zone related info to hook

--- src/shared/hooks/useZone.ts
+++ src/shared/hooks/useZone.ts
@@ -2,8 +2,13 @@ import { useEffect } from 'react';
 import { useWalletContext } from '../context/walletContext';
 import { Zone } from 'src/shared/types';
 
+import { Region, zoneRegionMap } from '../assets/regions';
+import { DomainName, zoneDomainNameMap } from '../constants/nodeData';
+
 export const useZone = (): {
   zone: Zone;
+  region?: Region;
+  domainName?: DomainName;
 } => {
   const { zone, getZone } = useWalletContext();
   useEffect(() => {
@@ -13,5 +18,7 @@ export const useZone = (): {
   }, [zone]);
   return {
     zone,
+    region: zone ? zoneRegionMap[zone] : undefined,
+    domainName: zone ? zoneDomainNameMap[zone] : undefined,
   };
 };

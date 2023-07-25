8b9996a09c571b3cf2a6cf93c90e697048d4ff51
juam <juanmanso@gmail.com>
2023-07-25 13:34:15 +0100

    chore: cleanup styles and apply final tweaks

--- src/main/home/receive/ReceiveAmountInputScreen.tsx
+++ src/main/home/receive/ReceiveAmountInputScreen.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, View, useColorScheme } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { useTranslation } from 'react-i18next';
 
-import { styledColors } from 'src/shared/styles';
 import ExchangeIcon from 'src/shared/assets/exchange.svg';
-import { useProfilePicture, useUsername, useWallet } from 'src/shared/hooks';
+import {
+  useProfilePicture,
+  useThemedStyle,
+  useUsername,
+  useWallet,
+} from 'src/shared/hooks';
 import { useAmountInput } from 'src/shared/hooks/useAmountInput';
 import {
   QuaiPayAvatar,
@@ -14,7 +18,7 @@ import {
   QuaiPayKeyboard,
   QuaiPayText,
 } from 'src/shared/components';
-import { Currency } from 'src/shared/types';
+import { Currency, Theme } from 'src/shared/types';
 import { abbreviateAddress } from 'src/shared/services/quais';
 import { useQuaiRate } from 'src/shared/hooks/useQuaiRate';
 
@@ -25,7 +29,7 @@ export const ReceiveAmountInputScreen: React.FC<
   ReceiveStackScreenProps<'ReceiveAmountInput'>
 > = ({ navigation }) => {
   const { t } = useTranslation();
-  const isDarkMode = useColorScheme() === 'dark';
+  const styles = useThemedStyle(themedStyle);
   const profilePicture = useProfilePicture();
   const username = useUsername();
   const wallet = useWallet();
@@ -43,46 +47,40 @@ export const ReceiveAmountInputScreen: React.FC<
   return (
     <QuaiPayContent title={t('common.request')}>
       <View style={styles.separator} />
-      <View style={styles.walletCardStyle}>
-        <View style={styles.nameAndPFPContainer}>
-          <QuaiPayAvatar
-            containerStyle={styles.image}
-            iconSize={60}
-            profilePicture={profilePicture ?? ''}
-          />
-          <QuaiPayText type="H3" style={styles.username}>
-            {username}
-          </QuaiPayText>
-          <QuaiPayText>{abbreviateAddress(wallet?.address)}</QuaiPayText>
-        </View>
-        <View style={styles.inputDisplayContainer}>
-          <QuaiPayInputDisplay
-            prefix={input.unit === 'USD' ? '$' : undefined}
-            suffix={` ${input.unit}`}
-            value={input.value}
-          />
-        </View>
-        <View style={styles.row}>
-          <QuaiPayText>
-            {eqInput.unit === 'USD' && '$'}
-            {eqInput.value} {eqInput.unit}
-          </QuaiPayText>
-          <QuaiPayButton
-            pill
-            outlined
-            type="secondary"
-            titleType="default"
-            title={input.unit}
-            onPress={onSwap}
-            style={styles.swapButton}
-            containerStyle={styles.swapButtonContainer}
-            RightIcon={
-              <ExchangeIcon
-                color={isDarkMode ? styledColors.white : styledColors.black}
-              />
-            }
-          />
-        </View>
+      <View style={styles.nameAndPFPContainer}>
+        <QuaiPayAvatar
+          containerStyle={styles.image}
+          iconSize={60}
+          profilePicture={profilePicture ?? ''}
+        />
+        <QuaiPayText type="H3" style={styles.username}>
+          {username}
+        </QuaiPayText>
+        <QuaiPayText>{abbreviateAddress(wallet?.address)}</QuaiPayText>
+      </View>
+      <View style={styles.inputDisplayContainer}>
+        <QuaiPayInputDisplay
+          prefix={input.unit === 'USD' ? '$' : undefined}
+          suffix={` ${input.unit}`}
+          value={input.value}
+        />
+      </View>
+      <View style={styles.row}>
+        <QuaiPayText>
+          {eqInput.unit === 'USD' && '$'}
+          {eqInput.value} {eqInput.unit}
+        </QuaiPayText>
+        <QuaiPayButton
+          pill
+          outlined
+          type="secondary"
+          titleType="default"
+          title={input.unit}
+          onPress={onSwap}
+          style={styles.swapButton}
+          containerStyle={styles.swapButtonContainer}
+          RightIcon={<ExchangeIcon color={styles.swapIcon.color} />}
+        />
       </View>
       <QuaiPayButton
         onPress={goToGeneratedQR}
@@ -99,46 +97,43 @@ export const ReceiveAmountInputScreen: React.FC<
   );
 };
 
-const styles = StyleSheet.create({
-  walletCardStyle: {
-    marginTop: 12,
-  },
-  row: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 8,
-    marginTop: 10,
-  },
-  swapButton: {
-    paddingVertical: 6,
-  },
-  swapButtonContainer: {
-    width: 75,
-  },
-  xUnit: {
-    textAlign: 'center',
-    marginTop: 5,
-    fontSize: 24,
-    color: styledColors.black,
-  },
-  continueButton: {
-    marginTop: 44,
-    marginHorizontal: 16,
-  },
-  inputDisplayContainer: {
-    marginTop: 16,
-  },
-  image: {
-    marginBottom: 0,
-  },
-  nameAndPFPContainer: {
-    alignItems: 'center',
-  },
-  username: {
-    marginTop: 8,
-  },
-  separator: {
-    flex: 1,
-  },
-});
+const themedStyle = (theme: Theme) =>
+  StyleSheet.create({
+    row: {
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 8,
+      marginTop: 10,
+    },
+    swapButton: {
+      paddingVertical: 6,
+    },
+    swapButtonContainer: {
+      width: 75,
+    },
+    swapIcon: {
+      color: theme.primary,
+    },
+    continueButton: {
+      marginTop: 40,
+      marginHorizontal: 16,
+      marginBottom: 8,
+    },
+    inputDisplayContainer: {
+      marginTop: 16,
+    },
+    image: {
+      marginBottom: 0,
+    },
+    nameAndPFPContainer: {
+      marginTop: 12,
+      alignItems: 'center',
+    },
+    username: {
+      marginTop: 8,
+    },
+    separator: {
+      flex: 1,
+    },
+  });
